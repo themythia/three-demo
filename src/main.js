c95115e3f7c9803852aefd3bddfc0491ba27fbb8
Emir Al <emir.al@havas.com>
2023-04-26 13:29:19 +0300

    spline-import

--- src/main.js
+++ src/main.js
@@ -1,167 +1,63 @@
 import * as THREE from 'three';
-import GUI from 'lil-gui';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import Stats from 'stats.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import SplineLoader from '@splinetool/loader';
 
-const parameters = {
-  screenSize: {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  },
-};
+// camera
+const camera = new THREE.OrthographicCamera(
+  window.innerWidth / -2,
+  window.innerWidth / 2,
+  window.innerHeight / 2,
+  window.innerHeight / -2,
+  -100000,
+  100000
+);
+camera.position.set(626.27, 431.79, 656.78);
+camera.quaternion.setFromEuler(new THREE.Euler(-0.48, 0.71, 0.33));
 
-const stats = new Stats();
-stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom);
-
-const gui = new GUI();
-const canvas = document.getElementById('three-canvas');
+// scene
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  parameters.screenSize.width / parameters.screenSize.height,
-  0.1,
-  100
+// spline scene
+const loader = new SplineLoader();
+loader.load(
+  'https://prod.spline.design/5elKCOlkLfcR0Uhr/scene.splinecode',
+  (splineScene) => {
+    scene.add(splineScene);
+  }
 );
-camera.position.set(4, 1, 2);
 
-const controls = new OrbitControls(camera, canvas);
+// renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
-const updateAllMaterials = () => {
-  scene.traverse((child) => {
-    if (
-      child instanceof THREE.Mesh &&
-      child.material instanceof THREE.MeshStandardMaterial
-    ) {
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
-  });
-};
+// scene settings
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 
-const gltfLoader = new GLTFLoader();
-gltfLoader.load('/model.glb', (gltf) => {
-  const model = gltf.scene.children[0].children[0];
-  model.position.set(0, 0, 0);
-  model.scale.set(0.1, 0.1, 0.1);
-  scene.add(model);
-  updateAllMaterials();
-});
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
-const directionalLight = new THREE.DirectionalLight('#ffffff', 0.6);
-
-directionalLight.position.set(5.5, 3, 4.5);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(512, 512);
-directionalLight.shadow.normalBias = 0.05;
-
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true,
-});
-renderer.setSize(parameters.screenSize.width, parameters.screenSize.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.physicallyCorrectLights = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.LinearToneMapping;
+scene.background = new THREE.Color('#2d2e32');
+renderer.setClearAlpha(1);
 renderer.toneMappingExposure = 3;
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-//helpers
-// y green
-// x red
-// z blue
-const axesHelper = new THREE.AxesHelper(5);
-axesHelper.position.set(0, 0, 0);
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight
-);
+// orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.125;
 
-scene.add(
-  camera,
-  ambientLight,
-  directionalLight,
-  axesHelper,
-  directionalLightHelper
-);
-
-gui
-  .add(directionalLight.position, 'x')
-  .min(-10)
-  .max(10)
-  .step(0.1)
-  .name('Directional Light Position X')
-  .onChange(() => directionalLightHelper.update());
-gui
-  .add(directionalLight.position, 'y')
-  .min(-10)
-  .max(10)
-  .step(0.1)
-  .name('Directional Light Position Y')
-  .onChange(() => directionalLightHelper.update());
-gui
-  .add(directionalLight.position, 'z')
-  .min(-10)
-  .max(10)
-  .step(0.1)
-  .name('Directional Light Position Z')
-  .onChange(() => directionalLightHelper.update());
-gui
-  .add(directionalLight, 'intensity')
-  .min(0)
-  .max(10)
-  .step(0.01)
-  .name('Directional Light Intensity');
-gui
-  .add(ambientLight, 'intensity')
-  .min(0)
-  .max(10)
-  .step(0.01)
-  .name('Ambient Light Intensity');
-gui.add(renderer, 'toneMapping', {
-  No: THREE.NoToneMapping,
-  Linear: THREE.LinearToneMapping,
-  Reinhard: THREE.ReinhardToneMapping,
-  Cineon: THREE.CineonToneMapping,
-  ACESFilmic: THREE.ACESFilmicToneMapping,
-});
-
-gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001);
-
-window.addEventListener('resize', () => {
-  // Update sizes
-  parameters.screenSize.width = window.innerWidth;
-  parameters.screenSize.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = parameters.screenSize.width / parameters.screenSize.height;
+window.addEventListener('resize', onWindowResize);
+function onWindowResize() {
+  camera.left = window.innerWidth / -2;
+  camera.right = window.innerWidth / 2;
+  camera.top = window.innerHeight / 2;
+  camera.bottom = window.innerHeight / -2;
   camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-  // Update renderer
-  renderer.setSize(parameters.screenSize.width, parameters.screenSize.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-const tick = () => {
-  // Update controls
+function animate(time) {
   controls.update();
-
-  stats.begin();
-
-  // monitored code goes here
-
-  stats.end();
-
-  // Render
   renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-//
-
-tick();
+}
